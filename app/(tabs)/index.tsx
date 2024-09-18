c09bfab4fb9fca0e9ab1519d6725c57554a82bbb
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-09-18 10:08:14 +0200

    task structure finished. To be continued...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,24 @@ import { Image, StyleSheet, Platform , View, Text} from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>To be continued...</Text>
+    <View style={{backgroundColor: "cyan",flex:1}}>
+      <View style={{backgroundColor: "red", height:150, justifyContent: "center", alignItems: "center"}}>
+      <Text style={{fontSize: 30}}>RÖD</Text>
+      </View>
+      <View style={{backgroundColor: "white", height:150, flexDirection: "row"}}>
+        <View style={{backgroundColor: "green", flex:1}}>
+          <Text style={{fontSize: 30}}>GRÖN</Text>
+        </View>
+        <View style={{backgroundColor: "yellow", flex:1}}></View>
+      </View>
+      <View style={{backgroundColor: "white", height:80, flexDirection: "row"}}>
+        <View style={{backgroundColor: "red", flex:3}}></View>
+        <View style={{backgroundColor: "black", flex:1}}></View>
+      </View>
+      <View style={{flex:1}}></View>
+      <View style={{ backgroundColor: "orange", height:50, alignItems: "flex-end", justifyContent: "center"}}>
+        <Text style={{fontSize: 30}}>ORANGE</Text>
+      </View>
     </View>
   );
 }
